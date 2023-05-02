09d2414c8056448f721e14d6f290502dcadad5ec
unknown <tanulo@t12g34.hunyadi.local>
2023-05-02 12:12:48 +0200

    Fixes

--- krajcar.py
+++ krajcar.py
@@ -1,10 +1,10 @@
-fajl=open("adatok.txt","w",encoding="utf-8")
+fajl=open("adatok.txt","a",encoding="utf-8")
+
 def atvalto():
     print("------------------------------------------")
     krajcar = int(input("Add meg a Forint mennyiséged: "))
     print("------------------------------------------")
     k = krajcar*100
-    kr = krajcar*60
 
     kuw = round(k/110700,4)
     bhd = round(k/90000,4)
@@ -17,23 +17,32 @@ def atvalto():
     eur = round(k/37300,4)
     usd = round(k/34000,4)
 
-    print(f"Konvenciós forint szerint Neked {kr} db krajcárod van!")
-    print(f"Osztrák forint szerint Neked {k} db krajcárod van!")
-    fajl.write(f"Konvenciós forint szerint Neked {kr} db krajcárod van!\n")
-    fajl.write(f"Osztrák forint szerint Neked {k} db krajcárod van!\n")
+    print(f"Forint szerint Neked {k} db krajcárod van!")
+    fajl.write(f"\n{krajcar}FT \t {k}KR")
 
     print("------------------------------------------------")
     print("-----------Top 10 Valuta-----------")
     print(f"Neked {kuw} db Kuwaiti dínárod van!")
+    fajl.write(f"\t{kuw}KD\t")
     print(f"Neked {bhd} db Bahreini dínárod van!")
+    fajl.write(f"\t{bhd}BD\t")
     print(f"Neked {omr} db Ománi rialod van!")
+    fajl.write(f"\t{omr}RO\t")
     print(f"Neked {jod} db Jordániai dínárod van!")
+    fajl.write(f"\t{jod}JD\t")
     print(f"Neked {gbp} db Angol fontod van!")
+    fajl.write(f"\t{gbp}GBP\t")
     print(f"Neked {gip} db Gibraltári fontod van!")
+    fajl.write(f"\t{gip}GIP\t")
     print(f"Neked {kyd} db Kajmán-Szigeteki dollárod van!")
+    fajl.write(f"\t{kyd}KYD\t")
     print(f"Neked {chf} db Svájci frankod van!")
+    fajl.write(f"\t{chf}CHF\t")
     print(f"Neked {eur} db Euród van!")
+    fajl.write(f"\t{eur}EUR\t")
     print(f"Neked {usd} db Dollárod van!")
+    fajl.write(f"\t{usd}USD\t")
+    print("------------------------------------------------")
 
 atvalto()
 
